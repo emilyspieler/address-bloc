46e6957a1add6b020f025e5cc513b0e41f831408
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-10-01 12:06:08 -0500

    prompt and menu assignment

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,11 +9,12 @@ const inquirer = require('inquirer');
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "Get Date",
           "Exit"
         ]
       }
     ];
-    this.contacts = [];   
+    this.contacts = [];
    }
 
    main(){
@@ -23,9 +24,12 @@ const inquirer = require('inquirer');
          case "Add new contact":
            this.addContact();
            break;
-         case "Exit":
+           case "Get Date":
+           this.getDate();
+           break;
+           case "Exit":
            this.exit();
-         default:
+           default:
            console.log("Invalid input");
            this.main();
        }
@@ -33,6 +37,26 @@ const inquirer = require('inquirer');
      .catch((err) => {
        console.log(err);
      });
+   }
+
+   getDate(){
+     this.clear();
+     var today = new Date();
+     var dd = today.getDate();
+     var mm = today.getMonth()+1; //January is 0!
+     var yyyy = today.getFullYear();
+
+     if(dd<10) {
+       dd = '0'+dd
+     }
+
+     if(mm<10) {
+       mm = '0'+mm
+     }
+
+     today = mm + '/' + dd + '/' + yyyy;
+     console.log(today);
+     this.main();
    }
 
    clear(){
